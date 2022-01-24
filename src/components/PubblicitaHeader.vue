ca6047b0d038d2e198469d542400a52cae0e4328
Guz117 <cristian.guzman117@gmail.com>
2022-01-24 12:02:26 +0100

    init

---
+++ src/components/PubblicitaHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+      <div>
+          <ul>
+              <li><a href="#">Download App</a></li>
+              <li><a href="#">iOS</a></li>
+              <li><a href="#">Advertise with us</a></li>
+          </ul>
+      </div>
+      <div>
+          <ul>
+              <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+              <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+              <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+              <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+          </ul>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'PubblicitaHeader'
+}
+</script>
+
+<style lang="scss">
+    ul {
+        list-style-type: none;
+        a {
+            text-decoration: none;
+        }
+    }
+</style>
