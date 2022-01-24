ceb3af0bdb3b081ddc1cdb69fe0087fbdf37cc6c
Guz117 <cristian.guzman117@gmail.com>
2022-01-24 18:06:24 +0100

    init jumbotron

---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="jumbotron">
+        <div class="jumbotron-text">
+            <div class="jumbotron-text-center">
+            <h1>Food Corner: Top Jappanese Restaurant for Shushi</h1>
+            <p>March 25, 2019</p>
+            </div>
+        </div>
+    </div>
+  
+</template>
+
+<script>
+export default {
+    name: 'Jumbotron'
+}
+</script>
+
+<style lang="scss">
+@import "../assets/scss/partials/_variables.scss";
+
+    .jumbotron {
+        height: 500px;
+        background: url('../assets/images/single-post-img3-1200x790.jpg');
+        background-size: cover;
+        background-position-y: center;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .jumbotron-text {
+        width: 40%;
+        padding: 2em 0;
+        background-color: $springWood;
+        display: flex;
+        justify-content: center;
+        .jumbotron-text-center {
+            width: 80%;
+            h1 {
+                text-align: center;
+            }
+            p {
+                padding-top: 1em;
+                text-align: center;
+            }
+        }
+    }
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,11 +1,19 @@
 <template>
-  <div></div>
+  <div>
+    <Jumbotron />
+  </div>
 </template>
 
 <script>
+import Jumbotron from './Jumbotron.vue'
+
 export default {
     name: 'Main',
+    components:{
+      Jumbotron,
+    }
 }
+
 </script>
 
 <style lang="scss">
