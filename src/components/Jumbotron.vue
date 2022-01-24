1aa1d49185ed71ee61796aa48c5cb1f07b1662e7
Guz117 <cristian.guzman117@gmail.com>
2022-01-24 21:25:11 +0100

    jambotreon

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -1,6 +1,17 @@
 <template>
     <div class="jumbotron">
+        <div class="icon">
+            <div class="demos">
+                <p><i class="fab fa-atlassian"></i></p>
+                <p class="text-icon">Demos</p>
+            </div>
+            <div class="sale">
+                <p><span class="dollar">$</span><span class="trentanove">39</span></p>
+                <p class="text-icon">On Sale</p>
+            </div>
+        </div>
         <div class="jumbotron-text">
+            <p class="pick">TODAY'S PICK</p>
             <div class="jumbotron-text-center">
             <h1>Food Corner: Top Jappanese Restaurant for Shushi</h1>
             <p>March 25, 2019</p>
@@ -27,6 +38,54 @@ export default {
         display: flex;
         justify-content: center;
         align-items: center;
+        position: relative;
+        .icon {
+            
+            position: absolute;
+            top: -90px;
+            right: 20px;
+
+            .text-icon {
+                font-size: 0.7em;
+                color: $nevada;
+            }
+
+            .fa-atlassian {
+                color: $fiord;
+            }
+
+            .demos, .sale {
+                width: 55px;
+                height: 55px;
+                text-align: center;
+                background-color: $springWood;
+                margin-bottom: 0.5em;
+                padding: 0.5em;
+                font-family: $fontFamily;
+                font-weight: bold;
+                border-radius: 5px;
+                box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+            }
+
+            .sale {
+                position: relative;
+
+                .dollar, .trentanove {
+                    color: $forestGreen;
+                }
+
+                .dollar {
+                    position: absolute;
+                    top: 5px;
+                    left: 5px;
+                }
+                .trentanove {
+                    font-size: 1.3em;
+                }
+            }
+
+            
+        }
     }
 
     .jumbotron-text {
@@ -35,6 +94,21 @@ export default {
         background-color: $springWood;
         display: flex;
         justify-content: center;
+        position: relative;
+        border-radius: 10px;
+        .pick {
+            position: absolute;
+            left: 50%;
+            top: -15px;
+            transform: translate(-50%);
+            padding: 5px 15px;
+            font-size: 0.7em;
+            font-weight: bold;
+            background-color: $orange;
+            border-radius: 5px;
+            font-family: $fontFamily;
+            color: $springWood;
+        }
         .jumbotron-text-center {
             width: 80%;
             h1 {
@@ -43,6 +117,7 @@ export default {
             p {
                 padding-top: 1em;
                 text-align: center;
+                color: $nevada;
             }
         }
     }
