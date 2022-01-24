661b6c5b6e63eafa98de472b0c14b1002fc87e9c
Guz117 <cristian.guzman117@gmail.com>
2022-01-24 12:02:49 +0100

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <PubblicitaHeader />
+  </div>
+</template>
+
+<script>
+import PubblicitaHeader from './PubblicitaHeader.vue'
+
+export default {
+    name: 'Header',
+    components: {
+        PubblicitaHeader
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
